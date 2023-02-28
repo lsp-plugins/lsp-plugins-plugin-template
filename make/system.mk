df2880fdcce7d37f1d5d1c4dceee57c1f3f1fb57
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-03-01 00:08:57 +0300

    Updated build

--- make/system.mk
+++ make/system.mk
@@ -58,10 +58,6 @@ ifeq ($(BUILD_ARCH),armel)
   override ARCHITECTURE   = $(BUILD_ARCH)
   ARCHITECTURE_FAMILY     = generic
   ARCHITECTURE_CFLAGS    :=
-else ifeq ($(BUILD_ARCH),armhf)
-  override ARCHITECTURE   = $(BUILD_ARCH)
-  ARCHITECTURE_FAMILY     = generic
-  ARCHITECTURE_CFLAGS    :=
 else ifeq ($(patsubst armv6%,armv6,$(BUILD_ARCH)),armv6)
   override ARCHITECTURE   = arm32
   ARCHITECTURE_FAMILY     = arm32
