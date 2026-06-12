3c5d5c52ca0276fa008936ab0afe42823b3e47ca
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-06-12 15:55:36 +0300

    Updated dependencies

--- modules.mk
+++ modules.mk
@@ -117,13 +117,13 @@ LSP_WS_LIB_URL_RO                       := https://github.com/lsp-plugins/$(LSP_
 LSP_WS_LIB_URL_RW                       := git@github.com:lsp-plugins/$(LSP_WS_LIB_NAME).git
 
 # Plugin-related module dependencies
-LSP_PLUGIN_FW_VERSION                   := 1.0.36
+LSP_PLUGIN_FW_VERSION                   := 1.0.37
 LSP_PLUGIN_FW_NAME                      := lsp-plugin-fw
 LSP_PLUGIN_FW_TYPE                      := src
 LSP_PLUGIN_FW_URL_RO                    := https://github.com/lsp-plugins/$(LSP_PLUGIN_FW_NAME).git
 LSP_PLUGIN_FW_URL_RW                    := git@github.com:lsp-plugins/$(LSP_PLUGIN_FW_NAME).git
 
-LSP_PLUGINS_SHARED_VERSION              := 1.0.35
+LSP_PLUGINS_SHARED_VERSION              := 1.0.36
 LSP_PLUGINS_SHARED_NAME                 := lsp-plugins-shared
 LSP_PLUGINS_SHARED_TYPE                 := src
 LSP_PLUGINS_SHARED_URL_RO               := https://github.com/lsp-plugins/$(LSP_PLUGINS_SHARED_NAME).git
